6e1d436cfcf1e64b344ef196c53d4284bc4b14cd
tracer-ysyx <tracer@ysyx.org>
2024-02-20 17:20:09 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  17:20:09 up 12:00,  4 users,  load average: 0.49, 0.38, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+const unsigned int MAX_SIM_TIME = 100;
+unsigned int sim_time = 0;
+
+int main(int argc, char** argv) {
+    Verilated::mkdir("logs");
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    // tfp->dumpvars(1, "t");  // trace 1 level under "t"
+    tfp->open("obj_dir/t_trace_top/simx.vcd");
+    while (!contextp->gotFinish() && contextp->time() < MAX_SIM_TIME) {
+        contextp->timeInc(1);
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        tfp->dump(contextp->time());
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+        sim_time++;
+    }
+    tfp->close();
+    delete top;
+    delete contextp;
+    return 0;
 }
