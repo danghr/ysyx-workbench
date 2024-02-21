eb939c95bf5e8372c9bf022ae873f099d8bc822b
tracer-ysyx <tracer@ysyx.org>
2024-02-21 18:02:47 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  18:02:47 up 23:05,  4 users,  load average: 0.14, 0.08, 0.06

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,7 +5,7 @@
 
 #define TRACE_NAME "./build/t_trace_top/top.vcd"
 // TOP_NAME in this file is defined by macro TOPNAME in npc/Makefile
-static TOP_NAME *dut;
+static TOP_NAME dut;
 
 const unsigned int MAX_SIM_TIME = 100;
 
@@ -15,14 +15,13 @@ int main(int argc, char **argv)
 {
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    dut = new TOP_NAME{contextp};
     Verilated::traceEverOn(true);
-    VerilatedVcdC *tfp = new VerilatedVcdC;
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    dut.trace(tfp, 99);
 
-    nvboard_bind_all_pins(dut);
+    nvboard_bind_all_pins(&dut);
     nvboard_init();
 
-    dut->trace(tfp, 99);
     tfp->open(TRACE_NAME);
 
     while (!contextp->gotFinish() && contextp->time() < MAX_SIM_TIME)
@@ -30,18 +29,17 @@ int main(int argc, char **argv)
         contextp->timeInc(1);
         int a = rand() & 1;
         int b = rand() & 1;
-        dut->a = a;
-        dut->b = b;
-        dut->eval();
+        dut.a = a;
+        dut.b = b;
+        dut.eval();
         tfp->dump(contextp->time());
-        printf("a = %d, b = %d, f = %d\n", dut->a, dut->b, dut->f);
+        printf("a = %d, b = %d, f = %d\n", dut.a, dut.b, dut.f);
         nvboard_update();
-        assert(dut->f == (dut->a ^ dut->b));
+        assert(dut.f == (dut.a ^ dut.b));
     }
     tfp->close();
     nvboard_quit();
     delete contextp;
     delete tfp;
-    delete dut;
     return 0;
 }
