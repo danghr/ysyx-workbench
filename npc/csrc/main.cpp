dc3f3a3b741076687fbf3c685759f18cc7cb4dd8
danghr <danghr@outlook.com>
2024-02-22 15:42:16 +0800

    Finish LED blinking & adding switchable trace

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,42 +1,64 @@
 #include <nvboard.h>
-#include "Vtop.h"
+#include TOP_NAME_H_FILE    // Defined in npc/Makefile
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
-// TOP_NAME defined in npc/Makefile
-static TOP_NAME *top;
+#define MAX_CYCLES 1e8
+#define MAX_SIM_TIME (MAX_CYCLES * 2)
+// #define DO_TRACE
 
-const unsigned int MAX_SIM_TIME = 1e6;
+static TOP_NAME *top;   // Defined in npc/Makefile
+VerilatedContext* contextp;
+VerilatedVcdC* tfp;
+
 
 void nvboard_bind_all_pins(TOP_NAME *top);
 
+void status_change() {
+    top->eval();
+#ifdef DO_TRACE
+    tfp->dump(contextp->time());
+#endif
+    nvboard_update();
+    contextp->timeInc(1);
+}
+
+void single_cycle() {
+    top->clk = 0; status_change();
+    top->clk = 1; status_change();
+}
+
+void reset(int n) {
+    top->rst = 1;
+    while(n-- > 0) single_cycle();
+    top->rst = 0;
+}
+
 int main(int argc, char **argv)
 {
-    VerilatedContext* contextp = new VerilatedContext;
+    contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     top = new TOP_NAME{contextp};
+#ifdef DO_TRACE
     Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp = new VerilatedVcdC;
+    tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
     // tfp->dumpvars(1, "t");  // trace 1 level under "t"
     tfp->open(TRACE_NAME);
+#endif
 
     nvboard_bind_all_pins(top);
     nvboard_init();
 
+    reset(10);
+
     while (!contextp->gotFinish() && contextp->time() < MAX_SIM_TIME) {
-        contextp->timeInc(1);
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-        tfp->dump(contextp->time());
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        nvboard_update();
-        assert(top->f == (a ^ b));
+        single_cycle();
     }
+#ifdef DO_TRACE
     tfp->close();
+    delete tfp;
+#endif
     delete top;
     delete contextp;
     nvboard_quit();
