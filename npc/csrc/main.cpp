f398df37a413c554c3b33e74ef70ffc42f3380b1
tracer-ysyx <tracer@ysyx.org>
2024-02-23 10:52:12 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 5.15.0-94-generic #104-Ubuntu SMP Tue Jan 9 15:26:57 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  10:52:12 up 24 min,  3 users,  load average: 0.16, 0.08, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,25 +5,32 @@
 
 #define MAX_CYCLES 1e8
 #define MAX_SIM_TIME (MAX_CYCLES * 2)
-// #define DO_TRACE
+// #define _DO_TRACE
+// #define _SEQUENTIAL_LOGIC
+// #define _NVBOARD
 
 static TOP_NAME *top;   // Defined in npc/Makefile
 VerilatedContext* contextp;
 VerilatedVcdC* tfp;
 
-
+#ifdef _NVBOARD
 void nvboard_bind_all_pins(TOP_NAME *top);
+#endif
 
 void status_change() {
     top->eval();
-#ifdef DO_TRACE
+#ifdef _DO_TRACE
     tfp->dump(contextp->time());
 #endif
+#ifdef _NVBOARD
     nvboard_update();
+#endif
     contextp->timeInc(1);
 }
 
+#ifdef _SEQUENTIAL_LOGIC
 void single_cycle() {
+
     top->clk = 0; status_change();
     top->clk = 1; status_change();
 }
@@ -33,13 +40,14 @@ void reset(int n) {
     while(n-- > 0) single_cycle();
     top->rst = 0;
 }
+#endif
 
 int main(int argc, char **argv)
 {
     contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     top = new TOP_NAME{contextp};
-#ifdef DO_TRACE
+#ifdef _DO_TRACE
     Verilated::traceEverOn(true);
     tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
@@ -47,20 +55,28 @@ int main(int argc, char **argv)
     tfp->open(TRACE_NAME);
 #endif
 
+#ifdef _NVBOARD
     nvboard_bind_all_pins(top);
     nvboard_init();
+#endif
 
+#ifdef _SEQUENTIAL_LOGIC
     reset(10);
+#endif
 
     while (!contextp->gotFinish() && contextp->time() < MAX_SIM_TIME) {
-        single_cycle();
+        assert(top->one == 1);
+        status_change();
     }
-#ifdef DO_TRACE
+
+#ifdef _DO_TRACE
     tfp->close();
     delete tfp;
 #endif
     delete top;
     delete contextp;
+#ifdef _NVBOARD
     nvboard_quit();
+#endif
     return 0;
 }
