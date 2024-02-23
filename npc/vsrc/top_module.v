e43dd04bd5ea35075f0427f592ce7eafded259a8
danghr <danghr@outlook.com>
2024-02-23 11:03:13 +0800

    HDLBits Output Zero

--- npc/vsrc/top_module.v
+++ npc/vsrc/top_module.v
@@ -1,6 +1,7 @@
-module top_module( output one );
+module top_module(
+    output zero
+);// Module body starts after semicolon
 
-// Insert your code here
-    assign one = 1'b1;
+  assign zero = 0;
 
 endmodule
