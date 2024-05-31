7a171deaf8e29d430cbc06bf690d18f2fc5825ab
tracer-ysyx <tracer@ysyx.org>
2024-05-31 15:39:20 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:39:20 up  6:45,  3 users,  load average: 1.25, 0.70, 0.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -129,19 +129,19 @@ int main(int argc, char **argv)
     srand(time(NULL));
 
     // Test Shifting Register
-    for (int i = 0; i < 1e4; i++) {
+    for (int i = 0; i < 1e0; i++) {
         int counter = 0;
         reset(5);
         uint8_t init_input_val = rand();
         top->reset = 1;
         top->init_val = init_input_val;
         single_cycle();
-        printf("Output val: %2x / Input val: %2x\n", top->out, init_input_val);
+        printf("Simulate %d / Output val: %2x / Input val: %2x\n", i, top->out, init_input_val);
         assert(top->out == init_input_val);
 
-        SIMULATE_UNTIL(counter >= 1e4) {
+        SIMULATE_UNTIL(counter >= 1e3) {
             counter++;
-            printf("Simulate %d-%d ", i, counter);
+            printf("Simulate %d-%d / ", i, counter);
             top->reset = 0;
             uint8_t prev_value = top->out;
             uint8_t now_input_val = rand() % 2;
