3404c1e87e521fb1d7c438a6d04d2447693850d8
tracer-ysyx <tracer@ysyx.org>
2024-06-07 17:39:34 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:39:34 up  1:19,  3 users,  load average: 0.25, 0.12, 0.09

--- npc/vsrc/top_module.v
+++ npc/vsrc/top_module.v
@@ -9,14 +9,13 @@ module top_module (
 );
 
     reg nextdata_n;
-    wire [7:0] ps2ctrl_data;
 
     ps2_keyboard ps2_keyboard_inst (
         .clk(clk),
         .resetn(~reset),
         .ps2_clk(ps2_clk),
         .ps2_data(ps2_data),
-        .data(ps2ctrl_data),
+        .data(data),
         .ready(ready),
         .nextdata_n(nextdata_n),
         .overflow(overflow)
@@ -24,14 +23,11 @@ module top_module (
 
     always @(posedge clk ) begin
         if (reset) begin
-            data <= 8'b0;
             nextdata_n <= 1'b1;
         end else if (ready & nextdata_n) begin
-            data <= ps2ctrl_data;
             nextdata_n <= 1'b0;
             $display("[PS/2 Keyboard Controller] Receive %x", data);
         end else begin
-            data <= data;
             nextdata_n <= 1'b1;
         end
     end
