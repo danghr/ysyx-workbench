c4b8b1b445ef835812e2ebeaf1d5da406b56ac86
tracer-ysyx <tracer@ysyx.org>
2024-06-14 17:14:52 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:14:52 up  6:49,  3 users,  load average: 0.14, 0.39, 0.43

--- npc/vsrc/ScancodeToASCII.v
+++ npc/vsrc/ScancodeToASCII.v
@@ -4,10 +4,10 @@ module ScancodeToASCII (
     output reg [7:0] ascii_code
 );
 
-    reg [7:0] scan_to_ascii[0:254];
+    reg [7:0] scan_to_ascii[0:255];
     
     initial begin
-        $readmemh("scancode_to_ascii.txt", scan_to_ascii);
+        $readmemh("/home/danghr/Desktop/YSYX/ysyx-workbench/npc/scancode_to_ascii.txt", scan_to_ascii);
         $display("Scancode test, should be 0x71: 0x%h", scan_to_ascii[21]);
     end
 
