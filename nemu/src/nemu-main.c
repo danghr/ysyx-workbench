a013a95b0485160dbdc9582a64e75eddbbf1a8b0
tracer-ysyx <tracer@ysyx.org>
2024-07-01 11:43:33 +0800

    > compile NEMU ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  11:43:33 up 1 day,  9:00,  3 users,  load average: 0.47, 0.35, 0.33

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,16 +20,24 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+// int main(int argc, char *argv[]) {
+//   /* Initialize the monitor. */
+// #ifdef CONFIG_TARGET_AM
+//   am_init_monitor();
+// #else
+//   init_monitor(argc, argv);
+// #endif
+
+//   /* Start engine. */
+//   engine_start();
+
+//   return is_exit_status_bad();
+// }
+
+extern word_t eval(char *e, bool *success);
+
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-#ifdef CONFIG_TARGET_AM
-  am_init_monitor();
-#else
-  init_monitor(argc, argv);
-#endif
-
-  /* Start engine. */
-  engine_start();
-
-  return is_exit_status_bad();
+  bool success = false;
+  printf("%ul\n", eval("1+2", &success));
+  return 0;
 }
