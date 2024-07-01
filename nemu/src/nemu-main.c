2f8e3bcbf03b6687f6aece9aac8cc85f48e9ec31
tracer-ysyx <tracer@ysyx.org>
2024-07-01 15:55:41 +0800

    > compile NEMU ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:55:41 up 1 day, 13:12,  3 users,  load average: 0.52, 0.32, 0.18

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -27,7 +27,7 @@ int check_expr(int argc, char *argv[]) {
   bool success = false;
   word_t result = expr("*0x80000000", &success);
   if (success)
-    printf("%x\n", result);
+    printf("0x%08x\n", result);
   else
     printf("Invalid expression\n");
   exit(0);
