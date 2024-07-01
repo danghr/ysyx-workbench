30dccde3cc990269d518bcf1b6b0aac1bd360764
tracer-ysyx <tracer@ysyx.org>
2024-07-01 16:53:28 +0800

    > compile NEMU ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:53:28 up 1 day, 14:10,  3 users,  load average: 0.34, 0.29, 0.27

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -28,7 +28,6 @@ int check_expr(int argc, char *argv[]) {
   word_t result = expr("*(0x80000000+-4--4)", &success);
   printf("%d\n", result);
   printf("%08x\n", result);
-  assert(result == 5+(-(2+3)));
   assert(success);
   exit(0);
 }
