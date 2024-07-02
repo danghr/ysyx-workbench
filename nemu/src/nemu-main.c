9a2b2e54fc30ac704bca405dcc52aba194f54eaa
Haoran Dang <danghr@outlook.com>
2024-07-02 16:44:33 +0800

    Finish expr testing

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,11 +14,70 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+
+extern word_t expr(char *e, bool *success);
+
+void check_expr(int argc, char *argv[]) {
+  char *nemu_home = getenv("NEMU_HOME");
+  char expr_file[1024];
+  strcpy(expr_file, nemu_home);
+  strcat(expr_file, "/tools/gen-expr/expr.txt");
+  FILE *fp = fopen(expr_file, "r");
+  assert(fp != NULL);
+
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int cnt = 0;
+  while ((read = getline(&line, &len, fp)) != -1) {
+    printf("Testing line %d\n", ++cnt);
+    printf("Retrieved line of length %zu:\n", read);
+    printf("%s", line);
+    char *ref_val_str = strtok(line, " ");
+#ifdef CONFIG_ISA64
+    word_t ref_val = strtoull(ref_val_str, NULL, 0);
+#else
+    word_t ref_val = strtoul(ref_val_str, NULL, 0);
+#endif
+    char *op = strtok(NULL, "\n");
+    bool success = false;
+    printf("Expression: \"%s\"\n", op);
+#ifdef CONFIG_ISA64
+    printf("Reference value: %lu\n", ref_val);
+#else
+    printf("Reference value: %u\n", ref_val);
+#endif
+    word_t result = expr(op, &success);
+    if (success) {
+      printf("Result: %u\n", result);
+      if (result == ref_val) {
+        printf("Result correct!\n");
+      } else {
+        printf("Result incorrect!\n");
+        assert(0);
+      }
+    } else {
+      printf("Expression invalid!\n");
+      assert(0);
+    }
+    printf("\n");
+  }
+  if (line)
+    free(line);
+
+  fclose(fp);
+  printf("All tests for expr passed!\n");
+  return;
+}
+
+#define CHECK_EXPR
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -28,8 +87,13 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
+#ifdef CHECK_EXPR
+  check_expr(argc, argv);
+  return 0;
+#else
   /* Start engine. */
   engine_start();
 
   return is_exit_status_bad();
+#endif
 }
