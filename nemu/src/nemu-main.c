f21380f2c3186006955a6dc3f26216501481d762
tracer-ysyx <tracer@ysyx.org>
2024-07-02 09:30:49 +0800

    > compile NEMU ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 11:19:33 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  09:30:49 up 1 day, 19:00,  3 users,  load average: 0.28, 0.31, 0.21

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -25,7 +25,7 @@ extern word_t expr(char *e, bool *success);
 int check_expr(int argc, char *argv[]) {
   init_monitor(argc, argv);
   bool success = false;
-  word_t result = expr("-(-3+2)+(-(-2-1))", &success);
+  word_t result = expr("((((((((((-(-3+2)+(-(-2-1))))))))))))", &success);
   printf("%d\n", result);
   printf("0x%08x\n", result);
   assert(success);
