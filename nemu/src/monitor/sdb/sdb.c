f9c9a47c2d92ae7e96e861b0ef566b096d08f488
tracer-ysyx <tracer@ysyx.org>
2024-07-20 10:46:12 +0800

    > compile NEMU ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 16:28:24 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  10:46:12 up  1:21,  3 users,  load average: 0.29, 0.26, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,7 @@ static int cmd_c(char *args) {
 
 static int cmd_q(char *args) {
   set_nemu_state(NEMU_QUIT, 0, 0);
+   // Still returns a negative number to exit the main loop. See `sdb_mainloop'.
   return -1;
 }
 
