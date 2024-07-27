adc6533a55282c2282bc762a16c308eed47bfbdb
tracer-ysyx <tracer@ysyx.org>
2024-07-27 11:02:57 +0800

    > sim RTL ysyx_22040000 党浩然 Linux danghr-vm 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 16:55:18 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  11:02:57 up  1:31,  3 users,  load average: 0.20, 0.07, 0.06

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,67 +23,9 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-extern CPU_state cpu;
-
 void isa_reg_display() {
-  for (int i = 0;
-       i < MUXDEF(CONFIG_RVE, 16, 32) /* See src/riscv32/include/isa_def.h */;
-       i++
-  ) {
-    /* 64-bit registers for RV64. See include/common.h and RISC-V ISA I Sec. 4.1 */
-#ifdef CONFIG_RV64  
-    printf("%-4s  0x%016x  %lld\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
-#else
-    printf("%-4s  0x%08x  %d\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
-#endif
-  }
-
-#ifdef CONFIG_RV64
-  printf("pc    0x%016x\n", cpu.pc);
-#else
-  printf("pc    0x%08x\n", cpu.pc);
-#endif
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  // We can accept both `$name` and `name` as register names.
-  // We can accept both `$ra` and `$x1` as register names.
-  // Also, we can accept `$x0`, `$0` and `$zero` for the zero register.
-  char *reg_name = (char *)s;
-  if (s[0] == '$')
-    reg_name++;
-  if (strcmp(reg_name, "pc") == 0) {
-    *success = true;
-    return cpu.pc;
-  }
-  if (reg_name[0] == 'x') {
-    // Directly extract number of the register
-    char *endptr;
-    long long idx = strtol(reg_name + 1, &endptr, 10);
-    if (!(*reg_name != '\0' && *endptr == '\0')) {
-      *success = false;
-      printf("Invalid register name: %s. Register names starting with 'x' can only be a number.\n", s);
-      return 0;
-    }
-    if (idx < 0 || idx >= MUXDEF(CONFIG_RVE, 16, 32)) {
-      *success = false;
-      printf("Invalid register name: %s. Register index out of range.\n", s);
-      return 0;
-    }
-    *success = true;
-    return cpu.gpr[idx];
-  }
-  if (strcmp(reg_name, "zero") == 0 || strcmp(reg_name, "0") == 0) {
-    *success = true;
-    return 0;
-  }
-  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
-    if (strcmp(reg_name, regs[i]) == 0) {
-      *success = true;
-      return cpu.gpr[i];
-    }
-  }
-  *success = false;
-  printf("Invalid register name: %s.\n", s);
   return 0;
 }
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -14,132 +14,30 @@
 ***************************************************************************************/
 
 #include "sdb.h"
-#include <cpu/watchpoint_check.h>
 
 #define NR_WP 32
 
-#ifdef CONFIG_WATCHPOINT
+typedef struct watchpoint {
+  int NO;
+  struct watchpoint *next;
+
+  /* TODO: Add more members if necessary */
+
+} WP;
+
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
-static int total_wp_count = 0;
-#endif
 
-#ifdef CONFIG_WATCHPOINT
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = 0;
+    wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
 
   head = NULL;
   free_ = wp_pool;
 }
-#endif
 
 /* TODO: Implement the functionality of watchpoint */
 
-WP* new_wp(const char *expression) {
-#ifdef CONFIG_WATCHPOINT
-  if (free_ == NULL) {
-    printf("No enough watchpoints.\n");
-    return NULL;
-  }
-  if (strlen(expression) >= 32) {
-    printf("Expression '%s' too long.\n", expression);
-    return NULL;
-  }
-  bool success = false;
-  word_t value = expr((char *)expression, &success);
-  if (!success) {
-    printf("Invalid expression '%s'.\n", expression);
-    return NULL;
-  }
-  WP *wp = free_;
-  free_ = free_->next;
-  wp->next = head;
-  wp->NO = ++total_wp_count;
-  strcpy(wp->str, expression);
-  wp->value = value;
-  printf("Watchpoint %d set. Initial value of '%s' is " FMT_WORD "\n", wp->NO, wp->str, wp->value);
-  head = wp;
-  return wp;
-#else
-  printf("Watchpoint not enabled. Recompile NEMU with config `watchpoint' enabled in menuconfig if you need.\n");
-  assert(0);  // This function should not be called by design
-  return NULL;
-#endif
-}
-
-void free_wp(int number) {
-#ifdef CONFIG_WATCHPOINT
-  if (number > total_wp_count) {
-    printf("Watchpoint %d does not exist.\n", number);
-    return;
-  }
-  WP *wp;
-  for (wp = head; wp != NULL; wp = wp->next) {
-    if (wp->NO == number) {
-      break;
-    }
-  }
-  if (wp == NULL) {
-    printf("Watchpoint %d does not exist.\n", number);
-    return;
-  }
-
-  if (head == wp) {
-    head = wp->next;
-  } else {
-    for (WP *p = head; p != NULL; p = p->next) {
-      if (p->next == wp) {
-        p->next = wp->next;
-        break;
-      }
-    }
-  }
-  printf("Watchpoint %d ('%s') deleted.\n", number, wp->str);
-  wp->NO = 0;
-  wp->next = free_;
-  free_ = wp;
-#else
-  printf("Watchpoint not enabled. Recompile NEMU with config `watchpoint' enabled in menuconfig if you need.\n");
-  assert(0);  // This function should not be called by design
-#endif
-}
-
-bool watchpoint_check() {
-#ifdef CONFIG_WATCHPOINT
-  bool ret = false;
-  for (WP *wp = head; wp != NULL; wp = wp->next) {
-    bool success = false;
-    word_t val = expr(wp->str, &success);
-    if (!success) {
-      printf("Invalid expression '%s'.\n", wp->str);
-      assert(0);  // Should not reach as we have checked the expression when adding watchpoint
-    }
-    if (val != wp->value) {
-      printf("Watchpoint %d: Value of '%s' changed from " FMT_WORD " to " FMT_WORD "\n", wp->NO, wp->str, wp->value, val);
-      wp->value = val;
-      ret = true;
-    }
-  }
-  return ret;
-#else
-  printf("Watchpoint not enabled. Recompile NEMU with config `watchpoint' enabled in menuconfig if you need.\n");
-  assert(0);  // This function should not be called by design
-  return false;
-#endif
-}
-
-void print_wp() {
-#ifdef CONFIG_WATCHPOINT
-  printf("Num   Expression\n");
-  for (WP *wp = head; wp != NULL; wp = wp->next) {
-    printf("%-5d %s\n", wp->NO, wp->str);
-  }
-#else
-  printf("Watchpoint not enabled. Recompile NEMU with config `watchpoint' enabled in menuconfig if you need.\n");
-  assert(0);  // This function should not be called by design
-#endif
-}
