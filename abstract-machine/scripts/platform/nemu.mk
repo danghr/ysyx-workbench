f15ef90e7a605c0b9674974c7b780df3606123ec
tracer-ysyx <tracer@ysyx.org>
2024-08-14 21:38:24 +0800

    > compile NEMU ysyx_24070014 党浩然 Linux danghr-vm 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 20:13:54 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  21:38:24 up  1:30,  1 user,  load average: 0.46, 0.42, 0.28

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -11,7 +11,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -b -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+NEMUFLAGS += --batch -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
@@ -27,3 +27,6 @@ run: image
 
 gdb: image
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+
+valgrind: image
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) valgrind ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
