b9dbe19c9080f9b8f25ca4cba408f7789095de43
tracer-ysyx <tracer@ysyx.org>
2024-09-06 17:07:07 +0800

    > compile NEMU ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:53:10 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:07:07 up 1 day, 14:10,  1 user,  load average: 0.30, 0.30, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -28,9 +28,11 @@ char *strncpy(char *dst, const char *src, size_t n) {
   size_t i;
   for (i = 0; i < n && src[i] != '\0'; i++)
     dst[i] = src[i];
-  if (src[i] != '\0')
+  if (src[i] != '\0') // Not enough characters in `src`
     for (; i < n; i++)
       dst[i] = '\0';
+  else // Enough characters in `src`
+    dst[i] = '\0';
   return dst;
 }
 
