f3386307d483de7f9e4757af3ac6a832522c15ad
tracer-ysyx <tracer@ysyx.org>
2024-09-09 17:34:00 +0800

    > compile NEMU ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:53:10 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  17:34:00 up 1 day, 16:53,  1 user,  load average: 0.37, 0.25, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -18,53 +18,53 @@ int printf(const char *fmt, ...) {
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   // Read the format string
-  // const int ERROR_RETURN = -1;
+  const int ERROR_RETURN = -1;
   int out_count = 0;
-  // for (const char *p = fmt; *p != '\0'; p++) {
-  //   // Handle normal characters
-  //   if (*p != '%') {
-  //     putchar_printf(*p, out, &out_count);
-  //     continue;
-  //   }
-  //   // Handle format specifiers
-  //   // Now we know that *p == '%'
-  //   if (*(p + 1) == 's') {
-  //     const char *s = va_arg(ap, const char *);
-  //     while (*s != '\0') {
-  //       putchar_printf(*(s++), out, &out_count);
-  //     }
-  //   } else if (*(p + 1) == 'd') {
-  //     int n = va_arg(ap, int);
-  //     // We cannot use `stoi` here as we are implementing a library function
-  //     // Output a negative symbol
-  //     if (n < 0) {
-  //       putchar_printf('-', out, &out_count);
-  //       n = -n;
-  //     }
-  //     // Use a buffer to store the digits
-  //     // Max: 2147483647, 10 digits
-  //     char buf[11];
-  //     buf[10] = '\0';
-  //     int loc = 9;
-  //     while (n > 0) {
-  //       buf[loc--] = n % 10 + '0';
-  //       n /= 10;
-  //     }
-  //     // Output the digits
-  //     for (int i = loc + 1; i < 10; i++) {
-  //       putchar_printf(buf[i], out, &out_count);
-  //     }
-  //   } else if (*(p + 1) == '\0') {
-  //     putstr("Invalid format specifier \%\n");
-  //     return ERROR_RETURN;
-  //   } else {
-  //     putstr("Invalid format specifier \%");
-  //     putch(*(p + 1));
-  //     putch('\n');
-  //     return ERROR_RETURN;
-  //   }
-  // }
-  // out[out_count] = '\0';
+  for (const char *p = fmt; *p != '\0'; p++) {
+    // Handle normal characters
+    if (*p != '%') {
+      putchar_printf(*p, out, &out_count);
+      continue;
+    }
+    // Handle format specifiers
+    // Now we know that *p == '%'
+    if (*(p + 1) == 's') {
+      const char *s = va_arg(ap, const char *);
+      while (*s != '\0') {
+        putchar_printf(*(s++), out, &out_count);
+      }
+    } else if (*(p + 1) == 'd') {
+      int n = va_arg(ap, int);
+      // We cannot use `stoi` here as we are implementing a library function
+      // Output a negative symbol
+      if (n < 0) {
+        putchar_printf('-', out, &out_count);
+        n = -n;
+      }
+      // Use a buffer to store the digits
+      // Max: 2147483647, 10 digits
+      char buf[11];
+      buf[10] = '\0';
+      int loc = 9;
+      while (n > 0) {
+        buf[loc--] = n % 10 + '0';
+        n /= 10;
+      }
+      // Output the digits
+      for (int i = loc + 1; i < 10; i++) {
+        putchar_printf(buf[i], out, &out_count);
+      }
+    } else if (*(p + 1) == '\0') {
+      putstr("Invalid format specifier \%\n");
+      return ERROR_RETURN;
+    } else {
+      putstr("Invalid format specifier \%");
+      putch(*(p + 1));
+      putch('\n');
+      return ERROR_RETURN;
+    }
+  }
+  out[out_count] = '\0';
   return out_count;
 }
 
