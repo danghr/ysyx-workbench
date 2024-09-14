4542ab12c0d1df50f2603ec8e7f1fde298c1213b
Haoran Dang <danghr@outlook.com>
2024-09-15 00:42:02 +0800

    Finish `sprintf`

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,59 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // Read the format string
+  const int ERROR_RETURN = -1;
+  int out_count = 0;
+  for (const char *p = fmt; *p != '\0'; p++) {
+    // Handle normal characters
+    if (*p != '%') {
+      out[out_count++] = *p;
+      continue;
+    }
+    // Handle format specifiers
+    // Now we know that *p == '%'
+    if (*(p + 1) == 's') {
+      const char *s = va_arg(ap, const char *);
+      while (*s != '\0') {
+        out[out_count++] = *(s++);
+      }
+      p++;
+    } else if (*(p + 1) == 'd') {
+      int n = va_arg(ap, int);
+      // We cannot use `stoi` here as we are implementing a library function
+      // Output a negative symbol
+      if (n < 0) {
+        out[out_count++] = '-';
+        n = -n;
+      }
+      // Use a buffer to store the digits
+      // Max: 2147483647, 10 digits
+      char buf[11];
+      buf[10] = '\0';
+      int loc = 9;
+      while (n > 0) {
+        buf[loc--] = n % 10 + '0';
+        n /= 10;
+      }
+      // Output the digits
+      for (int i = loc + 1; i < 10; i++) {
+        out[out_count++] = buf[i];
+      }
+      p++;
+    } else {
+      return ERROR_RETURN;
+    }
+  }
+  out[out_count] = '\0';
+  return out_count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
