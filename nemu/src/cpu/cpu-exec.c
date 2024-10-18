c83382ba807e32226e143e14b6a416901b21cfff
tracer-ysyx <tracer@ysyx.org>
2024-10-18 16:17:06 +0800

    > compile NEMU ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 16:09:00 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:17:06 up 16:36,  0 users,  load average: 0.35, 0.28, 0.18

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -143,8 +143,8 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-      // fall through
       iringbuf_print(&iringbuf);
+      // fall through
     case NEMU_QUIT: statistic();
   }
 }
