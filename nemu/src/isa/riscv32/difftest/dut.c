cbf538b64f8817c8b22d2c1489ace3d82844f513
tracer-ysyx <tracer@ysyx.org>
2024-11-01 15:50:59 +0800

    > compile NEMU ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 16:09:00 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  15:50:59 up 3 days, 21:59,  0 users,  load average: 0.11, 0.11, 0.04

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,7 +17,21 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+// CPU state for NEMU
+extern CPU_state cpu;
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  // ref_r: State copied from Spike
+  // pc: PC of the current instruction
+
+  for (int i = 0; i < MUXDEF(CONFIG_RV32E, 16, 32); i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      Log("Different value in register %s at pc = " FMT_WORD ", reference = " FMT_WORD ", NEMU = " FMT_WORD,
+          reg_name(i), pc, ref_r->gpr[i], cpu.gpr[i]);
+      return false;
+    }
+  }
+
   return false;
 }
 
