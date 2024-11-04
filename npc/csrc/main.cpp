dd8bfd534841a71a73403c627925924965cccc46
tracer-ysyx <tracer@ysyx.org>
2024-11-04 16:03:21 +0800

    > sim RTL ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 16:09:00 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:03:21 up 4 days,  6:52,  0 users,  load average: 0.26, 0.35, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -111,7 +111,7 @@ int main(int argc, char **argv)
 SIMULATE_BEGIN:
 	npc_state_run();
 	printf("NPC now running\n");
-	SIMULATE_UNTIL(npc_state.state == NPC_RUNNING);
+	SIMULATE_UNTIL(npc_state.state != NPC_RUNNING || SIMULATE_FINISHED)
 	{
 		printf("Reading instruction at PC 0x%08x\n", top->top_signal_pc);
 		top->top_signal_inst = paddr_read(top->top_signal_pc); // addi x1, x0, 1
