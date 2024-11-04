c1c441cbb09da16fa240fa7f0f22b9b333bb56e7
tracer-ysyx <tracer@ysyx.org>
2024-11-04 16:51:58 +0800

    > sim RTL ysyx_24070014 党浩然 Linux danghr-vm 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 16:09:00 UTC 2 aarch64 aarch64 aarch64 GNU/Linux  16:51:58 up 4 days,  7:40,  0 users,  load average: 0.32, 0.22, 0.17

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -75,6 +75,7 @@ void paddr_write(paddr_t addr, word_t data, byte mask) {
 }
 
 extern "C" int ysyx_24070014_paddr_read(int addr) {
+  printf("Addr: %d\n", addr);
   return (int)paddr_read((paddr_t)addr);
 }
 
